c229374a155175d30170e3f1446a9dcda0506c2e
aad1442 <bsse1442@iit.du.ac.bd>
2023-09-08 20:50:21 +0600

    Changed README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This is my first Software Project Lab(SPL)
 :::: "System Health Monitoring Tool "
+
+
+Understanding CPU usage in Linux : https://www.opsdash.com/blog/cpu-usage-linux.html
--- help/1_30Aug23/CPU_usages.cpp
+++ help/1_30Aug23/CPU_usages.cpp
@@ -2,20 +2,53 @@
 #include <thread>
 #include <chrono>
 #include <fstream>
+#include <vector>
 
 // Structure to store CPU usage information
 struct CpuUsageInfo {
-    double prevIdle;
-    double prevTotal;
-    double usage;
+    double user;
+    double nice;
+    double system;
+    double idle;
+    double iowait;
+    double irq;
+    double softirq;
+    double steal;
+    double guest;
+    double guest_nice;
 };
 
+// Function to parse CPU statistics from a line in /proc/stat
+bool parseCpuStats(const std::string& line, CpuUsageInfo& cpuInfo) {
+    return sscanf(line.c_str(), "cpu %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
+        &cpuInfo.user, &cpuInfo.nice, &cpuInfo.system, &cpuInfo.idle,
+        &cpuInfo.iowait, &cpuInfo.irq, &cpuInfo.softirq, &cpuInfo.steal,
+        &cpuInfo.guest, &cpuInfo.guest_nice) == 10;
+}
+
+// Function to calculate total CPU time
+double calculateTotalCpuTime(const CpuUsageInfo& cpuInfo) {
+    return cpuInfo.user + cpuInfo.nice + cpuInfo.system + cpuInfo.idle +
+           cpuInfo.iowait + cpuInfo.irq + cpuInfo.softirq + cpuInfo.steal +
+           cpuInfo.guest + cpuInfo.guest_nice;
+}
+
+// Function to calculate CPU usage percentage
+double calculateCpuUsagePercentage(const CpuUsageInfo& current, const CpuUsageInfo& previous) {
+    double currentTotal = calculateTotalCpuTime(current);
+    double previousTotal = calculateTotalCpuTime(previous);
+    double currentIdle = current.idle;
+    double previousIdle = previous.idle;
+
+    double cpuUsage = 100.0 * (1.0 - ((currentIdle - previousIdle) / (currentTotal - previousTotal)));
+
+    return cpuUsage;
+}
+
 // Function to calculate CPU usage
-void calculateCpuUsage(CpuUsageInfo &cpuInfo)
-{
+void calculateCpuUsage(CpuUsageInfo& cpuInfo) {
     std::ifstream statFile("/proc/stat");
-    if (!statFile.is_open())
-    {
+    if (!statFile.is_open()) {
         std::cerr << "Failed to open /proc/stat\n";
         return;
     }
@@ -23,55 +56,59 @@ void calculateCpuUsage(CpuUsageInfo &cpuInfo)
     std::string line;
     std::getline(statFile, line); // Read the first line (overall CPU stats)
 
-    // Extract CPU usage values from the line
-    long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
-    sscanf(line.c_str(), "cpu %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld",
-           &user, &nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
-
-    // Calculate total CPU time
-    double total = user + nice + system + idle + iowait + irq + softirq + steal;
-
-    // Calculate CPU usage percentage
-    double cpuUsage = 100.0 * (1.0 - ((idle - cpuInfo.prevIdle) / (total - cpuInfo.prevTotal)));
-
-    // Update the CPU usage info structure
-    cpuInfo.prevIdle = idle;
-    cpuInfo.prevTotal = total;
-    cpuInfo.usage = cpuUsage;
+    if (!parseCpuStats(line, cpuInfo)) {
+        std::cerr << "Failed to parse CPU statistics\n";
+        return;
+    }
 }
 
 // Function to monitor CPU usage for a specified number of iterations
-void monitorCpuUsage(int numIterations)
-{
-    CpuUsageInfo cpuInfo = {0.0, 0.0, 0.0};
-    std::ofstream outFile("cpu_usage.txt",std::ios::app);
+void monitorCpuUsage(int numIterations) {
+    std::vector<CpuUsageInfo> cpuStats;
+    cpuStats.reserve(numIterations);
 
-    if (!outFile.is_open())
-    {
-        std::cerr << "Failed to open the output file for writing\n";
-        return;
-    }
+    CpuUsageInfo cpuInfo;
+    CpuUsageInfo previousCpuInfo;
 
-    for (int i = 0; i < numIterations; ++i)
-    {
+    for (int i = 0; i < numIterations; ++i) {
         calculateCpuUsage(cpuInfo);
-        std::cout << "CPU Usage: " << cpuInfo.usage << "%\n";
+        cpuStats.push_back(cpuInfo);
 
-        // Write CPU usage to the file
-        outFile << "CPU Usage: " << cpuInfo.usage << "%" << std::endl;
+        double cpuUsage = 0.0;
 
-        if (i < numIterations - 1)
-        {
+        if (i > 0) {
+            cpuUsage = calculateCpuUsagePercentage(cpuInfo, previousCpuInfo);
+            std::cout << "Iteration " << i << ": CPU Usage - " << cpuUsage << "%\n";
+        }
+
+        // Update previous CPU info
+        previousCpuInfo = cpuInfo;
+
+        if (i < numIterations - 1) {
             // Wait for some time before calculating CPU usage again
             std::this_thread::sleep_for(std::chrono::seconds(1)); // Calculate every 1 second
         }
     }
 
+    // Write CPU usage statistics to a file
+    std::ofstream outFile("cpu_usage.txt");
+
+    if (!outFile.is_open()) {
+        std::cerr << "Failed to open the output file for writing\n";
+        return;
+    }
+
+    for (const CpuUsageInfo& info : cpuStats) {
+        outFile << "User: " << info.user << "%, Nice: " << info.nice << "%, System: " << info.system
+                << "%, Idle: " << info.idle << "%, IOWait: " << info.iowait << "%, IRQ: " << info.irq
+                << "%, SoftIRQ: " << info.softirq << "%, Steal: " << info.steal << "%, Guest: " << info.guest
+                << "%, Guest Nice: " << info.guest_nice << "%" << std::endl;
+    }
+
     outFile.close();
 }
 
-int main()
-{
+int main() {
     int numIterations = 5; // Specify the number of iterations
     monitorCpuUsage(numIterations);
     return 0;
